16fc817833f32a55cb30c6957eaa972c7f4f9e4e
sanojabby <sanobaptiste2018@gmail.com>
2022-11-17 02:19:30 -0800

    added

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,14 +1,39 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * @first: binary_tree_t
- * @second: binary_tree_t
+ * recursive_depth - measures the depth of a node in a binary tree
  *
- * Return: Binary Tree Node
+ * @tree: tree root
+ * Return: depth of a node in a binary tree
+ */
+size_t recursive_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (recursive_depth(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_uncle - finds the lowest common ancestor of two nodes
+ *
+ * @first: pointer to the first node
+ *  @second: pointer to the second node
+ *  Return: pointer to the lowest common ancestor
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		                    const binary_tree_t *second)
+		                      const binary_tree_t *second)
 {
-	return (0);
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	if (binary_tree_depth(first) > binary_tree_depth(second))
+		return (binary_trees_ancestor(first->parent, second));
+
+        if (binary_tree_depth(first) < binary_tree_depth(second))
+		return (binary_trees_ancestor(first, second->parent));
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
